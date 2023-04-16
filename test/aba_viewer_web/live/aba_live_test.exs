e8add64934c77127fbc4e20b87fd4f390c5933b9
alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>
2023-04-16 18:00:07 +0800

    test: fixed test
    Signed-off-by: alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>

--- test/aba_viewer_web/live/aba_live_test.exs
+++ test/aba_viewer_web/live/aba_live_test.exs
@@ -11,7 +11,9 @@ defmodule AbaViewerWeb.AbaLiveTest do
     test "lists all validator", %{conn: conn} do
       {:ok, _index_live, html} = live(conn, ~p"/")
 
-      assert html =~ "Upload file"
+      assert html =~ "Aba Viewer"
+      # assert html =~ "This tool will help you quickly identify issues with your ABA file."
+      # assert html =~ "Click to upload or drag and drop your ABA file"
     end
   end
 end
