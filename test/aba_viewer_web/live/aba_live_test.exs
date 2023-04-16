8374f5798664165126eb21a1d62a563343db149f
alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>
2023-04-16 20:36:08 +0800

    chore: format
    Signed-off-by: alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>

--- test/aba_viewer_web/live/aba_live_test.exs
+++ test/aba_viewer_web/live/aba_live_test.exs
@@ -3,10 +3,6 @@ defmodule AbaViewerWeb.AbaLiveTest do
 
   import Phoenix.LiveViewTest
 
-  @create_attrs %{}
-  @update_attrs %{}
-  @invalid_attrs %{}
-
   describe "Index" do
     test "lists all validator", %{conn: conn} do
       {:ok, _index_live, html} = live(conn, ~p"/")
