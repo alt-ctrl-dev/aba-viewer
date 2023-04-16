ddf541b06b9f3ced33f827b1107538b6deb3bf91
alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>
2023-04-16 15:51:06 +0800

    refactor: updated initial view
    Signed-off-by: alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>

--- lib/aba_viewer_web/live/aba_live/index.ex
+++ lib/aba_viewer_web/live/aba_live/index.ex
@@ -1,17 +1,11 @@
 defmodule AbaViewerWeb.AbaLive.Index do
   use AbaViewerWeb, :live_view
 
-
-
   @impl true
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
 
-  # @impl true
-  # def handle_params(params, _url, socket) do
-  #   {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  # end
 
   # @impl true
   # def handle_info({AbaViewerWeb.AbaLive.FormComponent, {:saved, aba}}, socket) do
