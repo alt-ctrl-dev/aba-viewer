f14402daacacb9d2898894046367c087f3831009
alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>
2023-04-16 14:49:20 +0800

    feat: added new live view
    Signed-off-by: alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>

--- lib/aba_viewer_web/router.ex
+++ lib/aba_viewer_web/router.ex
@@ -17,7 +17,7 @@ defmodule AbaViewerWeb.Router do
   scope "/", AbaViewerWeb do
     pipe_through(:browser)
 
-    get("/", PageController, :home)
+    live("/", AbaLive.Index, :index)
   end
 
   # Other scopes may use custom stacks.
---
+++ test/aba_viewer_web/live/aba_live_test.exs
@@ -0,0 +1,20 @@
+defmodule AbaViewerWeb.AbaLiveTest do
+  use AbaViewerWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  @create_attrs %{}
+  @update_attrs %{}
+  @invalid_attrs %{}
+
+
+
+  describe "Index" do
+
+    test "lists all validator", %{conn: conn} do
+      {:ok, _index_live, html} = live(conn, ~p"/")
+
+      assert html =~ "Listing Validator"
+    end
+  end
+end
