e94b4f4161e76074b26eae7ece0e0612645ccf1a
alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>
2023-04-18 21:33:51 +0800

    chore: added new packages
    Signed-off-by: alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -48,7 +48,9 @@ defmodule AbaViewer.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:aba_validator, "~> 2.1"}
+      {:aba_validator, "~> 2.1"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.12", only: [:dev, :test], runtime: false}
     ]
   end
 
